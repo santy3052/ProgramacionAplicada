22f5b2a8e13f9881fa88534f060697ff43292503
Juannieto1997 <47038637+Juannieto1997@users.noreply.github.com>
2022-06-18 12:47:20 -0500

    Initial commit

---
+++ INTRODUCCION/00-INTRODUCCION.py
@@ -0,0 +1,115 @@
+## Python
+'''
+Un lenguaje de programación permite dar instrucciónes a un computador de manera que este pueda realiazar una tarea.
+Existen distintos tipos de lenguajes de programación, los lenguajes compilados y los lenguajes interpretados
+lenguajes Compilados
+- requieren de un compilador, se revisan todos los elementos del código antes de realizar la ejecución
+- entre estos lenguajes se encuentran c++ y java
+lenguajes interpretados
+- se ejecuta cada linea de código de manera individual
+- entre estos lenguaes se encuentran python y matlab.
+En este curso utilizaremos python, uno de los lenguajes de programación más utilizados a nivel mundial.
+Adicionalmente como ambiente de desarrollo se utilizará la herramienta PyCharm, ya que estas ofrece distintas
+herramientas que permiten la facil implementación y corrección de los codigos, algunas de estas se verán más
+adelante.
+Finalmente, se hará uso de GIT una herramienta para el manejo de proyectos, esta herramienta es ampliamente
+utilizada en cualquier entorno de desarrollo y será de gran utilidad el manejo de esta
+'''
+## Objetivos del curso
+'''
+1. Dar a conocer herramientas que facilitan el desarrollo de programas de computador. 
+2. Comprender distintas estructuras de datos así como la utilidad y aplicaciones de estas.
+3. Entender distintas estrategias de diseño en programación
+'''
+## Métodología de evaluación y tematicas
+'''
+Según las normas de la universidad se contará con 3 cortes donde cada uno de estos corresponde a un porcentaje sobre
+la nota final
+Corte 1 = 30% 
+Corte 2 = 30%
+Corte 3 = 40%
+Total = 100%
+'''
+# Corte 1
+'''
+En el primer corte se realizará un repaso de los temas vistos anteriormente.
+- Herramientas de programación
+- variables 
+- Ecuaciones aritmeticas 
+- Tipos de datos
+- Cadenas de caracteres y sus operaciones
+- Estructuras basicas de python
+- condicionales 
+- ciclos
+- Manejo de Archivos
+En cuanto a la metodología de evaluación se tendran 4 actividades 
+1. examen practico (40%)
+2. examen teorico (40%)
+3. taller (15%)
+4. Quiz (5%)
+con Exepción del taller todas estas actividades se realizarán de manera INDIVIDUAL 
+adicionalmente contarán con tareas las cuales deben realizar pero no serán calificables. 
+'''
+# Corte 2
+'''
+En el segundo corte, se realizará una introducción al uso de objetos con python, así como a las 
+estructuras de datos.
+- Introducción a los objetos 
+    - herencia 
+    - herencia multiple 
+- Recursión
+- Métodos de busqueda 
+    -busqueda secuencial 
+    -Busqueda Binaria 
+- Algoritmos de ordenamiento 
+    - quick sort 
+    - Burbuja 
+    -...
+- Estructuras de datos 
+    - pilas 
+    - colas 
+    -listas 
+    -arboles 
+En cuanto a la metodología de evaluación se tendran 4 actividades 
+1. examen practico (40%)
+2. examen teorico (40%)
+3. taller (15%)
+4. Quiz (5%)
+con Exepción del taller todas estas actividades se realizarán de manera INDIVIDUAL 
+adicionalmente contarán con tareas las cuales deben realizar pero no serán calificables. 
+'''
+# Corte 3
+'''
+En el tercer corte se presentaran distintas metodologías para el diseño de programas y algoritmos
+así como enunciar erramientas de gran utilidad.
+- Programación Modular 
+- Programación Estructural 
+- Programación orientada a objetos 
+- Presentación de información (Graficas y manejo de datos)
+En cuanto a la metodología de evaluación se tendran 5 actividades 
+1. examen practico (10%)
+2. examen teorico (10%)
+3. taller (15%)
+4. Quiz (5%)
+5. proyecto Final (60%)
+con Exepción del taller todas estas actividades se realizarán de manera INDIVIDUAL 
+adicionalmente contarán con tareas las cuales deben realizar pero no serán calificables. 
+'''
+## SOBRE EL PROFESOR
+'''
+Juan Felipe Nieto 
+Magister en Ingeniería biomédica de la universidad de los andes 
+correo: jnieto@usbbog.edu.co
+Horario de tutorias: 
+Para las tutorias: antes de asistir enviar un correo con el tema que se tratará durante la tutoría, y asistir en 
+    en el horario a la sala de profesores de ingeniería mecatrónica.
+'''
+## NORMAS DE LA CLASE
+'''
+1. Asistir de manera puntual a las clases 
+2. Mantener un ambiente de respeto con todas las personas, tanto sus compañeros como con las multiples personas 
+    que hacen posible la clase 
+3. Entregar trabajo a tiempo y realizar el seguimieto de las actividades
+4. preguntar si se tienen dudas tanto durante como fuera de la clase
+5. Por NINGÚN motivo se recibirán trabajos fuera del tiempo establecido. 
+'''
---
+++ REPASO/01-HERRAMIETNAS.py
@@ -0,0 +1,65 @@
+## 01 - HERRAMIENTAS PARA LA PROGRAMACIÓN
+'''
+En esta sección revisaremos distintas herramietnas que facilitan el proceso de realizar los programas, así
+como manejar y realizar sus multiples trabajos
+'''
+## Pycharm
+'''
+Pycharm es un ambiente para el desarrollo de código y será la principal herramienta que estaremos usando a lo largo
+del semeste, este programa cuenta con distintas herramientas que facilitan el desarrollo e implementación de los 
+códigos. 
+en primer lugar se tiene la opción de instalar distintos plugins que facilitan la visualización de la información
+entre estos se encuentran 
+    - rainbow brackets: cambial el color de los parentesis de manera que se facilita leer la información al interior
+    de estos 
+    - rainbow indent: cambia el color de la indentación, esto permite verificar con gran facilidad en que sección 
+    se encuentra cada código 
+por otra parte cuenta con opciones que facilitan la depuración del código (elementos que estaremos abordando a 
+lo largo del curso) entre estas se encuentan
+    - la consola: permite correr con gran facilidad lineas individuales de código
+    - herramientas especificas para la depuración 
+    - celdas (plug-in): una herramienta que permite dividir el código en distintas secciones y ejecutarlas de 
+    manera individula. 
+para las instalación de plug-ins 
+File> Settings> plugins> "Buscar lo que se desea instalar"
+
+En cuanto al uso de este programa, es necesario crear un proyecto, lo que se puede hacer siguiendo las indicaciones
+que se encuentran en la pantalla inicial del programa, en caso de que se cree un proyecto sin un interprete 
+(el lenguaje de programación que se desea utilizar para ejecutar el código) se puede agregar haciendo click en 
+"add configuration" en la parte superior derecha del programa y siguiendo los pasos que se indican en el programa
+'''
+# más información
+'''
+NOTA: los dos primeros recursos se encuentran en INGLES
+1. https://www.youtube.com/watch?v=BPC-bGdBSM8&t=11s&ab_channel=JetBrainsTV 
+2. https://www.youtube.com/watch?v=HHcZbXsZtm0&ab_channel=CodeBucket
+3. https://www.jetbrains.com/es-es/pycharm/features/
+'''
+## StackOverflow
+'''
+StackOverflow es un sitio donde es posble publicar preguntas relacionadas con informatica y recibir una respuesta, 
+el objetivo de este sitio es encotrar distintas soluciones a problemas con los que se pueden llegar a encontrar
+lo más probable es que alguien sepa hacer lo que usteden estan intentando hacer o bien los pueda guiar en el 
+proceso. 
+Recuerden que esto es solo una herramienta, pueden ver un codigo que funciona pero es necesario que lo entiendan 
+antes de implementarlo, de lo contrario no estan aprendiendo y no progresarán. 
+metodología para usar esta herramietna 
+1. buscar la pregunta o error que les aparece 
+2. leer las primeras soluciones 
+3. implementar de manera textual la solución propuesta 
+4. modificar según las necesidades de su programa.
+'''
+## GIT
+'''
+GIT es un sitio para el manejo de repositorios, esto les permite regresar a versiones anteriores, revisar quíen 
+editó que segmento del código, así como guardar la información, para este curso se hará uso de esta herramienta 
+para el envio de trabajos así como para la comunicación y trabajos en grupo. 
+En primer lugar es necesario crear una cuenta en GITHub 
+https://github.com/github
+que será la plataforma que utilizaremos, para esto es necesario ingresar al link y seguir las instrucciones 
+que se presentan en la pagina para crear una cuenta nueva. 
+una vez se tenga una cuenta en GIThub 
+File> settings > version controll> Github > agregar su cuenta de Github
+
+
+'''
