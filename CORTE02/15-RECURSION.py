1cc20832f20f9216ec50862d7094247bfd4c4e5f
santy3052 <110635580+santy3052@users.noreply.github.com>
2022-09-12 22:19:45 -0500

    Update 15-RECURSION.py

--- CORTE02/15-RECURSION.py
+++ CORTE02/15-RECURSION.py
@@ -20,12 +20,43 @@ def f_contar(s_num): # se inicia la función, esta debe tener un parametro que s
 print(f_contar(1))
 
 # TODO: modificar esta función para que imprima todos los valores de manera ordenada
+def f_contar(s_num): # se inicia la función, esta debe tener un parametro que sirva como memoria del sistema
+    if s_num < 10: # si el número actual es menor que 10, aumentar en uno y volver a correr la función
+        print(s_num) # antes de agregarle unidades a la variable se imprime el numero
+        s_num+=1
+        return f_contar(s_num) # al correr nuevamente la función, ingresa con s_num + 1 como parametro por lo que cada vez incrementa
+    else:
+        return s_num # finalmetne cuando se cumple la condición se retorna el valor final
+print(f_contar(1))
 # TODO: modificar la función para que reciba dos parametros, el número inicial y el objetivo
+s_num = int(input('ingrese el numero de inicio'))
+f_num = int(input('ingrese su numero objetivo'))
+def f_contar(s_num, f_num): # se inicia la función, esta debe tener un parametro que sirva como memoria del sistema
+    if s_num < f_num: # si el número actual es menor que 10, aumentar en uno y volver a correr la función
+        print(s_num)
+        s_num+=1
+        return f_contar(s_num,f_num) # al correr nuevamente la función, ingresa con s_num + 1 como parametro por lo que cada vez incrementa
+    else:
+        return s_num # finalmetne cuando se cumple la condición se retorna el valor final
+print(f_contar(s_num, f_num))
+
 # TODO: modificar la función para que reciba 3 parametros el número inicial, el objetivo y los pasos entre números
+s_num = int(input('ingrese el numero de inicio'))
+f_num = int(input('ingrese su numero objetivo'))
+h_num = int(input('ingrese el salto numerico'))
+def f_contar(s_num, f_num, h_num): # se inicia la función, esta debe tener un parametro que sirva como memoria del sistema
+    if s_num < f_num: # si el número actual es menor que 10, aumentar en uno y volver a correr la función
+        print(s_num)
+        s_num+= h_num
+        return f_contar(s_num,f_num, h_num) # al correr nuevamente la función, ingresa con s_num + 1 como parametro por lo que cada vez incrementa
+    else:
+        return s_num # finalmetne cuando se cumple la condición se retorna el valor final
+print(f_contar(s_num, f_num, h_num))
+
 
 ## Torre de Hanoi
 '''
-existen problemas que son por naturaleza recursivos, un ejemplo de estos es la torre de hanoi 
+existen problemas que son por naturaleza recursivos, un ejemplo de estos es la torre de hanoi
 https://www.youtube.com/watch?v=vrXue8Lq1Ow&ab_channel=EdukativeS.L.-Rob%C3%B3ticaEducativaeningl%C3%A9s
 https://www.geogebra.org/m/NqyWJVra
 '''
